b44be47343d5252e10f29cf640d6ddb09b1436ab
smcgrath0 <smcgrath7777@gmail.com>
2019-08-21 12:45:35 -0700

    Define a stateful ProductList component in client/components/product-list.jsx that will render a grid of ProductListItems. Give it an initial state of products set to an empty Array.

--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ProductListItem from './product-list-item';
 
 export default class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
   render() {
     return (
-      <>
+      <div>
         <ProductListItem />
-      </>
+      </div>
     );
   }
 }
