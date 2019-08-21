dbff5c7e1594111be3a8e1b54db759bdc58b6f99
smcgrath0 <smcgrath7777@gmail.com>
2019-08-21 11:04:00 -0700

    Check out a branch from dev named view-products-front-end. Define an App class component in client/components/app.jsx. App will have no initial state for now. Define a stateless Header component in client/components/header.jsx that displays the store's name and logo. Add Header to App's render method.

---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class ProductListItem extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div>
+
+        </div>
+        <div>
+          <h3></h3>
+          <h5></h5>
+
+        </div>
+      </div>
+    );
+  }
+}
---
+++ client/components/product-list.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductListItem from './product-list-item';
+
+export default class ProductList extends React.Component {
+  render() {
+    return (
+      <>
+        <ProductListItem />
+      </>
+    );
+  }
+}
