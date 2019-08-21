37c3c2e540e25c5af936c3d27b216d75b6d4a26f
smcgrath0 <smcgrath7777@gmail.com>
2019-08-21 12:41:27 -0700

    Define a stateless ProductListItem component in client/components/product-list-item.jsx that renders a single product card.

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,14 +3,12 @@ import React from 'react';
 export default class ProductListItem extends React.Component {
   render() {
     return (
-      <div className="container">
-        <div>
-
-        </div>
-        <div>
-          <h3></h3>
-          <h5></h5>
-
+      <div className="container bg-secondary" style={{ width: '250px' }}>
+        <img src="./img/StockPhoto.png" style={{ height: '200px', width: '225px' }}></img>
+        <div className= "bg-danger">
+          <h3 >Product Name</h3>
+          <h5>$0.00</h5>
+          <p>Short description sit amet, consectetur adipiscing elit...</p>
         </div>
       </div>
     );
