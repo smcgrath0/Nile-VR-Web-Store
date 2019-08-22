179262e45ba1c82a739244ca946a5cf31d59d5df
smcgrath0 <smcgrath7777@gmail.com>
2019-08-22 15:54:22 -0700

    added http response code

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -3,6 +3,8 @@
 require_once("functions.php");
 require_once("db_connection.php");
 
+http_response_code(500);
+
 set_exception_handler('error_handler');
 
 $output = file_get_contents('dummy-products-list.json');
