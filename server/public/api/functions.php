30fb9583362a5357ea397c7041f9d213336ae849
smcgrath0 <smcgrath7777@gmail.com>
2019-08-22 16:00:32 -0700

    added http response code

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -2,6 +2,7 @@
 
 function error_handler($error){
   $output = ["success"=>false, "error"=>$error->getMessage()];
+  http_response_code(500);
   $json_output = json_encode($output);
   print($json_output);
   exit();
