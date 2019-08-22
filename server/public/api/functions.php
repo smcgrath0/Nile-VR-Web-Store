b0d063ad2c7c212df717dd8228a7e0020dfb2cf8
smcgrath0 <smcgrath7777@gmail.com>
2019-08-22 09:02:33 -0700

    cause an error by calling a non-existant function, doStuff. See if you get an error output. Make sure to do this AFTER the set_exeption_handler

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function error_handler($error){
-  $output = ["success"=>false, $error=>getMessage()];
+  $output = ["success"=>false, "error"=>$error->getMessage()];
   $json_output = json_encode($output);
   print($json_output);
   exit();
