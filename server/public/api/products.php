2d7700d79f984e87fe6c072c1204f5e9c30fd55b
smcgrath0 <smcgrath7777@gmail.com>
2019-08-22 15:42:48 -0700

    test your  variable. If it is falsey, then throw an exception, and send it your mysqli_connect_error return value.

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once("functions.php");
+require_once("db_connection.php");
 
 set_exception_handler('error_handler');
 
@@ -8,4 +9,11 @@ $output = file_get_contents('dummy-products-list.json');
 
 print($output);
 
+// header('Content-Type: application/json');
+
+// if (empty($_GET['id'])) {
+//   readfile('dummy-products-list.json');
+// } else {
+//   readfile('dummy-product-details.json');
+// }
 ?>
