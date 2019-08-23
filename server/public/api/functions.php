e25b7326fb67e4f447555a776def084b9f184c9c
smcgrath0 <smcgrath7777@gmail.com>
2019-08-22 18:31:07 -0700

    read up about sending headers in the startup function, send the following header: Content-type:application/json this will let the receiving client know that you are sending json data in the top of your products.php file, after you set your error handler, call your startup function in postman, check the headers you are receiving and see if you got the right one

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -1,5 +1,9 @@
 <?php
 
+function startUp(){
+  header( "Content-type:application/json");
+}
+
 function error_handler($error){
   $output = ["success"=>false, "error"=>$error->getMessage()];
   http_response_code(500);
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -5,9 +5,28 @@ require_once("db_connection.php");
 
 set_exception_handler('error_handler');
 
-$output = file_get_contents('dummy-products-list.json');
+startUp();
 
-print($output);
+$query = "SELECT * FROM `products`";
+
+$result = $conn->query($query);
+
+if ($result = mysqli_query($conn, $query)) {
+  $output = ["success" => "success", "data" => []];
+
+
+  while ($row = $result->fetch_assoc()) {
+    $output["data"][] = $row;
+  }
+  if (mysqli_num_rows($result)  === 0) {
+    print("no data availiable");
+    exit();
+  }
+  print(json_encode($output["data"]));
+} else {
+  printf("Error message: %s\n", mysqli_error($conn));
+  exit();
+}
 
 // header('Content-Type: application/json');
 
