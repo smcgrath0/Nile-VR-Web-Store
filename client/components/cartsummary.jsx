3693390452c5c968440561cf63242fa6e99554d5
smcgrath0 <smcgrath7777@gmail.com>
2019-08-24 12:49:14 -0700

    got screen fully functional to start, now need to be able to add items to cart

--- client/components/cartsummary.jsx
+++ client/components/cartsummary.jsx
@@ -2,17 +2,35 @@ import React from 'react';
 import CartSummaryItem from './cartsummaryitem.jsx';
 
 export default class CartSummary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.calculatetotal = this.calculatetotal.bind(this);
+  }
+  calculatetotal() {
+    var total = 0;
+    for (var i = 0; i < this.props.cart.length; i++) {
+      for (var j = 0; j < this.props.cart[i].count; j++) {
+        total += parseFloat((this.props.cart[i].price / 100).toFixed(2));
+      }
+    }
+    return total.toFixed(2);
+  }
   render() {
-    // console.log(this.props.cart);
     return (
       <>
+        <button className="btn bg-primary mb-2 ml-5" onClick={ () => {
+          this.props.setView('catalog', { });
+        }}>Go back to Catalog</button>
         <div className="container bg-dark text-light" style={{ height: '100vh', width: '100vh' }}>
-          My Cart
+          <h1>My Cart</h1>
           {this.props.cart.map(item => {
             return (
               <CartSummaryItem key={item.id} item={item} />
             );
           })}
+          <div className="bg-light text-dark mt-5">
+            <h4>Cart Total: ${this.calculatetotal()}</h4>
+          </div>
         </div>
       </>
     );
