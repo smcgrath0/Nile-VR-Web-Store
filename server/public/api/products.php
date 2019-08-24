e207bf5eed72f81ada934228af12b75ba9c04b99
smcgrath0 <smcgrath7777@gmail.com>
2019-08-24 15:04:32 -0700

    more unnesting ifs

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -16,21 +16,22 @@ if(!empty($_GET['id']) && !is_numeric($_GET['id'])){
   $query = "SELECT * FROM `products` WHERE `id` = " . $_GET['id'];
 }
 
-if ($result = mysqli_query($conn, $query)) {
-  $output = [];
+$result = mysqli_query($conn, $query);
 
-  while ($row = mysqli_fetch_assoc($result)) {
-    $output[] = $row;
-  }
-
-  if (mysqli_num_rows($result) === 0 && !empty($_GET['id'])) {
-    throw new Exception('invalid ID: ' . $_GET['id']);
-  }
-  
-  print(json_encode($output));
-
-} else {
+if(!$result){
   throw new Exception("Error message: %s\n", mysqli_error($conn));
 }
+
+$output = [];
+
+while ($row = mysqli_fetch_assoc($result)) {
+  $output[] = $row;
+}
+
+if (mysqli_num_rows($result) === 0 && !empty($_GET['id'])) {
+  throw new Exception('invalid ID: ' . $_GET['id']);
+}
+
+print(json_encode($output));
 
 ?>
