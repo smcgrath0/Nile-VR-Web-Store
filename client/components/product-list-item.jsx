455eb78c5fa43d01c9954a605aee806f26baba43
smcgrath0 <smcgrath7777@gmail.com>
2019-10-10 11:11:11 -0700

    fixed product images, so no strech

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -4,7 +4,9 @@ export default class ProductListItem extends React.Component {
   render() {
     return (
       <div className="container pt-3 mr-2 mt-4 pb-1 rounded view" style={{ width: '250px', height: '600px', backgroundColor: '#FFFFFF' }}>
-        <img src={this.props.image} className="rounded" style={{ height: '200px', width: '225px' }}></img>
+        <div className="rounded productItemImg" style={{ backgroundImage: 'url(' + this.props.image + ')' }}>
+
+        </div>
         <div className="p-2 mt-2 mr-2 rounded" style={{ width: '225px' }}>
           <h3>{this.props.name}</h3>
           <h5>${(this.props.price / 100).toFixed(2)}</h5>
